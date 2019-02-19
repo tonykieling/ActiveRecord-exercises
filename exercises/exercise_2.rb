88927eee2772321887063d0f03432835f4229101
Tony Kieling <tony.kieling@gmail.com>
2019-02-19 22:22:14 +0000

    exercise 2 is done

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id: 1)
+puts "store1 = #{@store1.name}"
+@store1.update(name: "NEW NAME FOR BURNABY STORE")
+# another way to do so
+# @store1.name = "+NEW NAME FOR BURNABY STORE+"
+# @store1.save
+
+
+@store2 = Store.second
+puts "store2 = #{@store2.name}"
+# i = Image.find :first
+
