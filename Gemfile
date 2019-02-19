860d0c1ba2ee7c3295b34de1656a89e9431bbfb3
Tony Kieling <tony.kieling@gmail.com>
2019-02-19 22:02:32 +0000

    exercise 1 is done

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@
 source "https://rubygems.org"
 
 gem 'pry' # for debugging
+# gem 'pg', '~> 0.20'
 gem 'pg'
 gem 'activerecord'
+
--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,19 @@
 require_relative '../setup'
+require_relative '../lib/store'
 
 puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+Burnaby = Store.create name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true
+
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+Richmond = Store.create name: "Richmond", annual_revenue: 1260000, womens_apparel: true
+
+# # Gastown (annual_revenue of 190000 carries men's apparel only)
+Gastown = Store.create name: "Gastown", annual_revenue: 190000, mens_apparel: true
+
+puts "Number of STORES:  #{Store.count}"
+
