ea5bf236845361df5302d75c79d8765d6fe8c61f
Tony Kieling <tony.kieling@gmail.com>
2019-02-20 03:42:14 +0000

    exercise 7 is done

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,10 +1,10 @@
 require_relative '../setup'
-# require_relative './exercise_1'
-# require_relative './exercise_2'
-# require_relative './exercise_3'
-# require_relative './exercise_4'
-# require_relative './exercise_5'
-# require_relative './exercise_6'
+require_relative './exercise_1'
+require_relative './exercise_2'
+require_relative './exercise_3'
+require_relative './exercise_4'
+require_relative './exercise_5'
+require_relative './exercise_6'
 
 puts "Exercise 7"
 puts "----------"
