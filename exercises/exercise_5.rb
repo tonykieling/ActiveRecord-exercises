58e02b014dcac3bab0b13f26af0aff34a8b9e718
Tony Kieling <tony.kieling@gmail.com>
2019-02-20 01:07:00 +0000

    exercise 5 is done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -13,10 +13,11 @@ puts "# Total revenue for the entire company (all stores):  $ #{Store.sum("annua
 
 puts "# Average annual revenue for all stores:  $ #{Store.average("annual_revenue")}\n"
 
-number_stores_generating_more_than_1M = Store.where("annual_revenue >= 1000000")
-puts "# Store(s) generating more than $1M annually:"
-count = 1
-number_stores_generating_more_than_1M.map do |store|
-  puts "#{count}- Store: #{store.name}  -  Annual Sales: $ #{store.annual_revenue}"
-  count += 1
-end
+number_stores_generating_more_than_1M = Store.where("annual_revenue >= 1000000").count
+puts "# Store(s) generating more than $1M annually: #{number_stores_generating_more_than_1M}"
+
+# count = 1
+# number_stores_generating_more_than_1M.map do |store|
+#   puts "#{count}- Store: #{store.name}  -  Annual Sales: $ #{store.annual_revenue}"
+#   count += 1
+# end
