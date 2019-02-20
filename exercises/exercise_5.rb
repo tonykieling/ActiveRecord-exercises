1cc6f6d4a02af86e65fc2aa2c4c7039873870c96
Tony Kieling <tony.kieling@gmail.com>
2019-02-20 00:46:11 +0000

    exercise 5 is done

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+puts "# Total revenue for the entire company (all stores):  $ #{Store.sum("annual_revenue")}\n"
+
+puts "# Average annual revenue for all stores:  $ #{Store.average("annual_revenue")}\n"
+
+number_stores_generating_more_than_1M = Store.where("annual_revenue >= 1000000")
+puts "# Store(s) generating more than $1M annually:"
+count = 1
+number_stores_generating_more_than_1M.map do |store|
+  puts "#{count}- Store: #{store.name}  -  Annual Sales: $ #{store.annual_revenue}"
+  count += 1
+end
